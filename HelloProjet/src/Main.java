2c961baebaba3afcdc3251b65bd467559e4c8a2d
mspc <zj1258419087@gmail.com>
2020-07-05 16:26:45 +0800

    a

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
 
 public class Main {
-
-    @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+    public static void main(String[] args) {
+        System.out.println("a");
     }
 }
